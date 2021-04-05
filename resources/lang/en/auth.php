5858b16add1a99fa7f8009b065c669726922fd8f
Burak YILDIRIM <yildirimbrk@gmail.com>
2021-04-05 21:54:10 +0300

    minor değişiklikler
    kendi veritabanına bağlanması için gerekli ayarlar yapıldı. Giriş hataları türkçeleştirildi.

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Girdiğiniz bilgiler kayıtlarımızla eşleşmiyor.',
+    'password' => 'Parolanızı kontrol ederek tekrar deneyiniz.',
+    'throttle' => 'Çok fazla giriş denemesi. Lütfen :seconds saniye içerisinde tekrar deneyiniz.',
 
 ];
