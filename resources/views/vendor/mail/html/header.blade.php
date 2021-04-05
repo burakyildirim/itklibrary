8ef5284dd272620937f2562a16eff2afedf1e887
Burak YILDIRIM <yildirimbrk@gmail.com>
2021-04-06 00:28:39 +0300

    parolamı unuttum türkçeleştirme

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="http://142.93.143.135/images/itk_arma.png" class="logo" alt="İTK Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
