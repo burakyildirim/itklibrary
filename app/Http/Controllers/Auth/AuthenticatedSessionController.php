6d9018ccb81f1943705866d3688e1f860b69dfe8
Burak YILDIRIM <yildirimbrk@gmail.com>
2021-04-09 02:11:48 +0300

    uygulama ismi config deki parametre kullanıldı

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -50,5 +52,40 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+
+    public function google(){
+        // google oauth'a isteği gönderiyorum.
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googleRedirect(){
+        // google dan gelen oauth tepkisini sitede login session yaratmak için kullanıyorum.
+
+        try {
+            $user = Socialite::driver('google')->user();
+        } catch (\Exception $e) {
+            return redirect('/login');
+        }
+        // sadece itk.k12.tr mail adresiyle gelen kayıtları kabul ediyorum.
+        if(explode("@", $user->email)[1] !== 'itk.k12.tr'){
+            return redirect()->to('/');
+        }
+        // böyle bir kullanıcı varsa login page geri yönlendiriyorum.
+        $existingUser = User::where('email', $user->email)->first();
+        if($existingUser){
+            // eğer böyle bir kullanıcı varsa login ettiriyorum.
+            auth()->login($existingUser, true);
+        } else {
+            // yoksa yeni bir kullanıcı kaydı oluşturuyorum.
+            $newUser                  = new User;
+            $newUser->name            = $user->name;
+            $newUser->email           = $user->email;
+            $newUser->google_id       = $user->google_id;
+            $newUser->avatar          = $user->avatar;
+            $newUser->save();
+            auth()->login($newUser, true);
+        }
+        return redirect()->to('/');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
             </a>
         </x-slot>
         <p class="text-lg text-black font-semibold mb-3 text-center">
@@ -10,42 +10,51 @@
         </p>
 
         <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+        <x-auth-session-status class="mb-4" :status="session('status')"/>
 
         <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+        <x-auth-validation-errors class="mb-4" :errors="$errors"/>
 
         <form method="POST" action="{{ route('login') }}">
-            @csrf
+        @csrf
 
-            <!-- Email Address -->
+        <!-- Email Address -->
             <div>
-                <x-label for="email" :value="__('E-Posta')" />
+                <x-label for="email" :value="__('E-Posta')"/>
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                         autofocus/>
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Parola')" />
+                <x-label for="password" :value="__('Parola')"/>
 
                 <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                         type="password"
+                         name="password"
+                         required autocomplete="current-password"/>
             </div>
 
             <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                    <input id="remember_me" type="checkbox"
+                           class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                           name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Beni hatırla') }}</span>
                 </label>
             </div>
+            <div class="flex items-center mt-2">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('loginGoogle') }}">
+                    {{ __('Google Hesabıyla Oturum Açın') }}
+                </a>
+            </div>
+            <div class="flex items-center justify-end mt-2">
 
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                       href="{{ route('password.request') }}">
                         {{ __('Parolanızı mı unuttunuz?') }}
                     </a>
                 @endif
