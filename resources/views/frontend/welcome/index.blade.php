deb2e3a6c0aa7a3c507beb99737a86ababdd5aef
Burak YILDIRIM <yildirimbrk@gmail.com>
2021-05-08 10:57:53 +0300

    anasayfa wording change

--- resources/views/frontend/welcome/index.blade.php
+++ resources/views/frontend/welcome/index.blade.php
@@ -122,7 +122,7 @@
             </span>
         </h1>
 
-        <p class="lead">Aradığınız kitabın hangi kütüphanelerimizde olduğunu öğrenmek için hızlı ve etkili bir araç.
+        <p class="lead">Aradığınız kitabın hangi kütüphanemizde olduğunu öğrenmek için hızlı ve etkili bir araç.
             İstediğiniz kitap için rezervasyon yaptırabilir ve ilgili kütüphanemizden ödünç alabilirsiniz. Kitap
             okuyarak kazandığınız puanlarla sahip olabileceğiniz ödüller sizleri bekliyor! <span
                 style="color:#cf352d;"><i class="fas fa-heart"></i></span> <span style="color:#2684b7;"><i
